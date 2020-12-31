b6e2b46a85231fa1933dfab9c7c179555d3787da
Christopher McKerley <cmckerley@gmail.com>
2020-12-31 17:14:16 -0500

    Update Google Script

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxYmWOh94Mi1IllhiiP_fNNtBxPHs7Gm0Tx8YkUYC-zrLEUJi8/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx5gwtWUKFLxBn4PQ7rDJI4HuSYjnpAxI-2leqvIwChbmBtn1Pkk0eg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
